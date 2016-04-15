cf2d3d97df6ddd866009e7b4c820cb4fed1fa4d7
Alex <aaswenson@wisc.edu>
2016-04-14 20:59:37 -0500

    continued coding, added reprocessing efficiency

--- reprocessing.py
+++ reprocessing.py
@@ -12,6 +12,7 @@ file = open('outp')
 mass_flow = parameters.mass
 time_step = parameters.time
 fuel_mat  = parameters.fuel
+reprocessing_efficiency = parameters.efficiency
 
 mat_dat = file_parse(file)
 
@@ -20,6 +21,12 @@ actindes = mat_dat['Actinides']
 carrier = mat_dat['Carrier Material']
 fission_products = mat_dat['Fission Products']
 
-mass_frac_fp = sum(fission_products.itervalues())
+mass_frac_fp = sum(fission_products.itervalues())  # determine mass fraction of fission products
 
+mass_frac_fuel = mass_frac_fp*reprocessing_efficiency   # convert fps into fertile fuel, taking into account efficency
 
+fuel_dict = {'92238':mass_frac_fuel}    # load fuel 
+fp_dict = fission_products*(1-reprocessing_efficiency)
+
+actinides = actinides + fuel_dict                   # update mass fractions for fuel and fission products
+fission_products = fission_products + fp_dict
