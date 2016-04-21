9b93d98251288232deea1a661fcdc921ef2269c0
AJ Gross <ajgross2@wisc.edu>
2016-04-21 12:01:41 -0500

    Changed some rounding to try and conserve mass

--- reprocessing_RevI.py
+++ reprocessing_RevI.py
@@ -9,6 +9,7 @@ def fraction_stay(par,t):
 def reprocessing(dict_mass, f_stay, mat, sigma_lib = '73c'):
     tot_i = 0
     m_gone = 0
+    round_precision = 4
     dict_wf = {mat: {'Actinides':{}, 'Carrier Material':{}, 'Fission Products':{} }}
     for subsubdict in dict_mass[mat]:
         for subsubsubdict in dict_mass[mat][subsubdict]:
@@ -16,16 +17,16 @@ def reprocessing(dict_mass, f_stay, mat, sigma_lib = '73c'):
     for subsubsubdict in dict_mass[mat]['Fission Products']:
         m_gone = m_gone + dict_mass[mat]['Fission Products'][subsubsubdict]*(1-f_stay)
         dict_mass[mat]['Fission Products'][subsubsubdict] = dict_mass[mat]['Fission Products'][subsubsubdict]*f_stay
-        wf = dict_mass[mat]['Fission Products'][subsubsubdict]/tot_i
+        wf = round(dict_mass[mat]['Fission Products'][subsubsubdict], round_precision)/tot_i
         dict_wf[mat]['Fission Products'][subsubsubdict] = mcnp_line(subsubsubdict, wf, sigma_lib)
     for subsubsubdict in dict_mass[mat]['Actinides']:
-        wf = dict_mass[mat]['Actinides'][subsubsubdict]/tot_i
+        wf = round(dict_mass[mat]['Actinides'][subsubsubdict], round_precision)/tot_i
         dict_wf[mat]['Actinides'][subsubsubdict] = mcnp_line(subsubsubdict, wf, sigma_lib)
     for subsubsubdict in dict_mass[mat]['Carrier Material']:
-        wf = dict_mass[mat]['Carrier Material'][subsubsubdict]/tot_i
+        wf = round(dict_mass[mat]['Carrier Material'][subsubsubdict], round_precision)/tot_i
         dict_wf[mat]['Carrier Material'][subsubsubdict] = mcnp_line(subsubsubdict, wf, sigma_lib)
     dict_mass[mat]['Actinides']['92238'] = dict_mass[mat]['Actinides']['92238'] + m_gone
-    wf = dict_mass[mat]['Actinides']['92238']/tot_i
+    wf = round(dict_mass[mat]['Actinides']['92238'], round_precision)/tot_i
     dict_wf[mat]['Actinides']['92238'] = mcnp_line('92238', wf, sigma_lib)
     return dict_mass, dict_wf
 
