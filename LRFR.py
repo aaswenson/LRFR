4619208ad2bb28b0884eb23b8aec315c269dc6fb
Alexander A Swenson <aaswenson@wisc.edu>
2016-04-21 20:00:34 -0500

    working rev, calls MCNP for designated amount of times

--- LRFR.py
+++ LRFR.py
@@ -2,14 +2,63 @@
 # This script is dependent on mcnp6 
 
 
-from parameters import inputfile
+from parameters import inputfile 
+#, intervals, cores
 import os
 import subprocess
+import reprocessing_RevI
+import input_parse
+import output_parse
 
 inputFile = inputfile
+intervals = 2
+cores = '4'
+args = [cores]
 
-commandList = ["mcnp6","i="+inputFile, "tasks 16"]
 
-subprocess.run(commandList)
+def mcnp_call(inputfile,args,i):
+    arg = args[0]
+    i = 'i'
+    run_command = ["mcnp6","i="+inputFile,"o=interval"+ i , "tasks "+arg]
+    subprocess.run(run_command)
+ 
+    
+    
 
-subprocess.run(["rm", "--f","outp"]) 
+
+
+for i in range(0,intervals):
+    mcnp_call(inputFile,args,i)
+    subprocess.run(["ls"])
+    
+# this loop will roll through the burnup interval, running MCNP and saving outputs along the way
+    #inputFile = inputmakerdohicky(inputfile)
+
+        
+    
+
+
+
+
+# Begin pseudo code for loopz and such
+# import input file
+# grab time step from intervals
+
+
+
+
+
+# Things this programs should do
+# ls the directory, pass it in and look for existing MCNP output files
+    # if they are there, throw exception, warning of potential data loss
+
+
+
+
+
+
+# THINGS AN INPUT WRITER SHOULD DO
+
+# change the burn card so the user can define different intervals (higher BOC fidelity) 
+# change materials (obviously)
+
