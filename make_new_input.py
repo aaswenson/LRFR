c8e2289311e59a03e189003d1062a75d1961e665
Riley <ribenson@wisc.edu>
2016-04-24 22:04:05 -0500

    The function currently runs without any errors but doesn't do what it's supposed to do. It appears it can't find the line that indicates the beginning of the material that needs to be changed

--- make_new_input.py
+++ make_new_input.py
@@ -1,47 +1,52 @@
 #===============================================================================
-# Functions to take copy of old input file and add in new material compositions
+# Function to take copy of old input file and add in new material compositions
 #===============================================================================
+import re
 
-def update_inp_mats(input_file_cp, reprocess_dict, param):
+
+def update_inp_mats(input_file_cp, reprocess_dict, mat_num):
     # Make new mcnp input material string
     new_mat = []
-    for subsubdict in reprocess_dict:
-        for subsubsubdict in reprocess_dict[param.mat][subsubdict]:
-            mat_line = reprocess_dict[param.mat][subsubdict][subsubsubdict]
+    for subsubdict in reprocess_dict[mat_num]:
+        for subsubsubdict in reprocess_dict[mat_num][subsubdict]:
+            mat_line = reprocess_dict[mat_num][subsubdict][subsubsubdict]
             new_mat.append(mat_line)
 
     # Now loop through file, record input file before and after old material
     orig_before = []
     orig_after = []
-    with open('input_file_cp', 'r+') as inp_file:
+    with open(input_file_cp, 'r+') as inp_file:
         for line in inp_file:
-            row = inp_file.readline()
-            row_split = row.split()
-            if row_split[0] == 'm'+str(param.mat):
+            line_split = line.split()
+            if line_split == 'm'+mat_num:
+                print('I found the material to be changed')
                 break
             else:
-                orig_before.append(row)
+                orig_before.append([line])
         
         for line in inp_file:
             if line[0] == ' ':
                 pass
             else:
-                orig_after.append(line)
+                orig_after.append([line])
                 break
         
         orig_after.append(inp_file.readlines())
 
         # Now rewrite file with new material in place of old fuel material
         for i in range(len(orig_before)):
-            inp_file.write(orig_before[i])
-        
-        for i in range(len(new_mat)):
-            inp_file.write(new_mat[i])
+            inp_file.write(orig_before[i][0])
+
+        new_mat_line_one = 'm' + mat_num + new_mat[0][0]
+        inp_file.write(new_mat_line_one)
+
+        for i in range(1,len(new_mat)):
+            inp_file.write(new_mat[i][0])
 
         for i in range(len(orig_after)):
-            inp_file.write(orig_after[i])
+            inp_file.write(str(orig_after[i]))
         
-    
+        inp_file.flush()
 
 
                 
