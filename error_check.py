ff2015711343073dd85732a521538c2751d08bfa
Alex <aaswenson@wisc.edu>
2016-04-29 02:21:00 -0500

    more work on error handling, write missing isotopes to input file, not quite there yet

--- error_check.py
+++ error_check.py
@@ -3,6 +3,7 @@
 # This module checks the runs of MCNP for errors, reacts accordingly 
 
 import re
+import fileinput
 
 def check_for_missingXS(outfile):
     file = open(outfile)
@@ -10,9 +11,10 @@ def check_for_missingXS(outfile):
     f = str('fatal error.   cross-section tables missing for zaid =')
     for line in file:
         if f in line:
-            XS = re.findall(r'\d+',line)
-            errors.append(XS)
-    return errors
+            XS = re.findall(r'\b\d+\b',line)
+            errors.extend(XS)
+            omit_add = len(errors)
+    return errors, omit_add
 
 
 def write_omit_number(inputfile,omit_add,error_check_number):  
@@ -38,10 +40,47 @@ def write_omit_number(inputfile,omit_add,error_check_number):
     file.close()
     return newfile
 
+def add_missing_isotopes(inputfile_cp,inputfile_name,errors):
+    file_old = open(inputfile_cp,'r')
+    file_new = open(inputfile_name,'w')
+    s = 'omit='
+    for line in file_old:
+        file_new.write(line)
+        if line[0:4] == ' ' and s in line:
+            for i in range(len(errors)):
+                omitted_isotope = errors[i]
+                file_new.write("omitted_isotope\n")
+    file_old.close()
+    file_new.close()
+    
+           
+          
+        
+                
+            
+
+  
+            
+            
+            
+
 # filename = 'interval_0o'
 # errors = check_for_missingXS(filename)
 # print(errors)
 
-inputfile = 'errorcheck.i'
-newfile = write_omit_number(inputfile,24)
-print(newfile)
+#inputfile = 'errorcheck.i'
+#newfile = write_omit_number(inputfile,24,'1')
+#print(newfile)
+
+errors , omit_add = check_for_missingXS('interval_0o')
+print(errors)
+write_omit_number('errorcheck.i',omit_add,'1')
+add_missing_isotopes('inputfile_cp1','errorcheck.i',errors)
+
+
+
+
+
+
+
+
