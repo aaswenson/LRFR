78308ce9dbdce206481d22f697557c230bdc3075
AJ <ajgross2@wisc.edu>
2016-05-10 22:32:20 -0500

    Added a parameters file for testing purposes only

---
+++ parameters_for_tests.py
@@ -0,0 +1,20 @@
+#----------------------------------------------------#
+#---These are for testing purposes, DO NOT EDIT!!!---#
+#----------------------------------------------------#
+
+
+# Total Core Volume [m^3]
+# Must be a float or integer
+V = 28
+
+# Reprocessing rate [L/day]
+# Must be a float
+V_dot = float(40)
+# Converting to [m^3/day]
+V_dot = V_dot/1000
+
+# Reprocessing Efficiency 
+# Must be an integer or float
+eta_reprocessing = 1.0
+
+
